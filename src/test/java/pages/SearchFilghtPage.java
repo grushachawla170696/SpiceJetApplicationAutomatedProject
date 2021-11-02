e13f8b114306adfaeca6b6dcb8a316f2807fd7f2
grushachawla170696 <93521618+grushachawla170696@users.noreply.github.com>
2021-11-02 16:45:28 +0530

    Update SearchFilghtPage.java

--- src/test/java/pages/SearchFilghtPage.java
+++ src/test/java/pages/SearchFilghtPage.java
@@ -20,6 +20,10 @@ public class SearchFilghtPage
 	By desti_loc_btn=By.xpath("//div[@class='css-76zvg2 r-cqee49 r-ubezar r-1kfrs79'][contains(text(),'Agartala')]");
 	By departure_dt_select=By.xpath("//div[@class='css-76zvg2 r-jwli3a r-ubezar r-16dba41']");
 	By return_dt_select=By.xpath("//body/div[@id='react-root']/div[@id='main-container']/div[@class='css-1dbjc4n r-1niwhzg r-13awgt0 r-1jgb5lz r-r0h9e2 r-13qz1uu']/div[@class='css-1dbjc4n r-14lw9ot']/div[@class='css-1dbjc4n r-14lw9ot r-z2wwpe r-vgw6uq r-156q2ks r-urutk0 r-8uuktl r-136ojw6']/div[@class='css-1dbjc4n r-1pcd2l5 r-1uwte3a r-m611by r-bnwqim']/div[@class='css-1dbjc4n r-19h5ruw r-136ojw6']/div[@style='flex: 1 1 0%;']/div[@class='css-1dbjc4n r-1niwhzg r-z2wwpe r-17b9qp5 r-1g94qm0 r-h3f8nf r-u8s1d r-u3yave r-8fdsdq']/div[@class='css-1dbjc4n r-14lw9ot r-11u4nky r-rs99b7 r-6koalj r-eqz5dr r-1pi2tsx r-pm9dpa r-1knelpx r-13qz1uu']/div[@class='css-1dbjc4n r-1euycsn']/div[@class='css-1dbjc4n r-150rngu r-18u37iz r-16y2uox r-1wbh5a2 r-lltvgl r-buy8e9 r-1sncvnh']/div[@class='css-1dbjc4n r-18u37iz']/div[3]/div[1]/div[3]/div[2]/div[3]/div[1]/div[1]");
+	By passenger_btn=By.xpath("//*[contains(text(),'Passengers')]");
+	By adultsel_btn=By.xpath("//body/div[@id='react-root']/div[@id='main-container']/div[@class='css-1dbjc4n r-1niwhzg r-13awgt0 r-1jgb5lz r-r0h9e2 r-13qz1uu']/div[@class='css-1dbjc4n r-14lw9ot']/div[@class='css-1dbjc4n r-14lw9ot r-z2wwpe r-vgw6uq r-156q2ks r-urutk0 r-8uuktl r-136ojw6']/div[@class='css-1dbjc4n r-1pcd2l5 r-1uwte3a r-m611by r-bnwqim']/div[@class='css-1dbjc4n r-18u37iz r-19h5ruw r-184en5c']/div[@class='css-1dbjc4n']/div/div[@class='css-1dbjc4n r-14lw9ot r-z2wwpe r-1rjd0u6 r-1g94qm0 r-h3f8nf r-u8s1d r-13qz1uu r-8fdsdq']/div[@class='css-1dbjc4n r-150rngu r-eqz5dr r-16y2uox r-1wbh5a2 r-11yh6sk r-1rnoaur r-1sncvnh']/div[@class='css-1dbjc4n']/div[@class='css-1dbjc4n r-k8qxaj r-d9fdf6']/div[1]/div[2]/div[3]/*[1]");
+
+
 	By currency_btn=By.xpath("//*[contains(text(),'Currency')]");
 	By gbp_currency_select=By.xpath("//*[starts-with(text(),'GBP')]");
 	By stud_radio_btn=By.xpath("//*[contains(text(),'Students')]");
